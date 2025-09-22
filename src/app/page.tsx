8f983e8485239accbcd7dab80c583475161c889d
lucasmacielg <lucasmaciel395@gmail.com>
2025-09-22 20:33:59 -0300

    Fix: Melhorando pagina inicial.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-col items-center justify-center h-full py-2 gap-4">
-        <h1 className="text-4xl font-bold">Primeiro projeto usando <span className="text-green-600">Next.JS</span> de Lucas.</h1>
-        <h2 className="text-2xl font-semibold">Praticando...</h2>
-      </div>
+    <div className="flex flex-col items-center gap-4 justify-center h-screen gap-4">
+      <h1 className="text-4xl font-bold">Primeiro projeto usando <span className="text-green-600">Next.JS</span> de Lucas.</h1>
+      <h2 className="text-2xl font-semibold">Praticando...</h2>
     </div>
   );
 }
